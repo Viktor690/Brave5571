eb3f1d9ed544bdaeae00aa1dd8b7db4852d7ed91
Viktor <burjatvic@gmail.com>
2023-12-27 00:42:38 +0800

    Added second task

--- Program.cs
+++ Program.cs
@@ -30,3 +30,23 @@
 // {
 //     Console.WriteLine("Нет, " + firstNumber + " не является кв. от числа " + secondNumber);
 // }
+
+
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// N = 4
+int negativeN = N * -1; // -4
+
+while (negativeN <= N)
+{
+    Console.Write(negativeN + " ");
+    negativeN++; // negativeN = negativeN + 1
+}
